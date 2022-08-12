33c8b7fed128c96ece4f001ad53218a9a101f7a3
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-08-12 10:36:14 +0530

    mt3

---
+++ MTConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MTConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigData bigData = new BigData();
+            //bigData.Fill();
+            //bigData.Fill();
+            Parallel.Invoke(bigData.Fill, bigData.Fill);
+            Console.WriteLine($"Count: {bigData.data.Count}");
+        }
+    }
+    class BigData
+    {
+        public Stack<int> data = new Stack<int>();
+        public void Fill()
+        {
+            for (int i = 1; i <= 1000000; i++)
+            {
+                data.Push(i);
+            }
+        }
+    }
+}
