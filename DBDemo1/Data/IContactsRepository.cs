6b80d9b64c24a883b5d42cc7459b4505b0c5759f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-08-18 12:42:48 +0530

    ado

---
+++ DBDemo1/Data/IContactsRepository.cs
@@ -0,0 +1,14 @@
+﻿using DBDemo1.Entities;
+
+namespace DBDemo1.Data
+{
+    public interface IContactsRepository
+    {
+        void Save(Contact contactToSave);
+        void Delete(int id);
+
+        Contact GetContact(int id);
+
+        List<Contact> GetAllContacts();
+    }
+}
---
+++ DBDemo1/Program.cs
@@ -0,0 +1,99 @@
+﻿using DBDemo1.Data;
+using DBDemo1.Entities;
+
+namespace DBDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contact Manager
+            // Save
+            // Select
+            // Delete
+            // Edit
+
+            // Code First
+            // DB First
+
+
+            // 1. Choose the database/create 
+            // 2. Create table
+            // Collect contact information from user and insert into db
+
+            while (true)
+            {
+                Console.WriteLine("My Contacts Management App");
+                Console.WriteLine("1. Save Contact");
+                Console.WriteLine("2. Delete Contact");
+                Console.WriteLine("3. Search Contact");
+                Console.WriteLine("4. Display All Contacts");
+
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1: AcceptContact(); break;
+                    case 2: DeleteContact(); break;
+                    case 3: SearchContact(); break;
+                    case 4: DisplayAllContacts(); break;
+                    default:
+                        break;
+                }
+            }
+
+
+        }
+
+        static void DisplayAllContacts()
+        {
+            IContactsRepository repo = new ContactRepository();
+            List<Contact> contacts = repo.GetAllContacts();
+
+            Console.WriteLine("Contact Id \t Name \t Mobile \t Email \t Location");
+
+            foreach (Contact c in contacts)
+            {
+                Console.WriteLine($"{c.ContactID} \t {c.Name.Trim()} \t {c.Mobile.Trim()}\t{c.Email.Trim()}\t{c.Location.Trim()}");
+            }
+        }
+        static void SearchContact()
+        {
+            Console.WriteLine("Enter Contact ID to search");
+            int cid = int.Parse(Console.ReadLine());
+            IContactsRepository repo = new ContactRepository();
+            Contact c = repo.GetContact(cid);
+
+            Console.WriteLine($"Contact ID : {c.ContactID}");
+            Console.WriteLine($"Name : {c.Name}");
+            Console.WriteLine($"Mobile : {c.Mobile}");
+            Console.WriteLine($"Email : {c.Email}");
+            Console.WriteLine($"Location : {c.Location}");
+        }
+        static void DeleteContact()
+        {
+            Console.Write("Enter Contact id to delete");
+            int id = int.Parse(Console.ReadLine());
+            IContactsRepository repo = new ContactRepository();
+            repo.Delete(id);
+        }
+
+        static void AcceptContact()
+        {
+            Contact c = new Contact();
+            Console.Write("Enter Contact Name: ");
+            c.Name = Console.ReadLine();
+            Console.Write("Enter Mobile: ");
+            c.Mobile = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            c.Email = Console.ReadLine();
+            Console.Write("Enter Location: ");
+            c.Location = Console.ReadLine();
+            IContactsRepository repo = new ContactRepository();
+            repo.Save(c);
+
+
+        }
+    }
+}
