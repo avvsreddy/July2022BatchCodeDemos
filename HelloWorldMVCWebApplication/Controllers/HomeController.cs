1d433ac60029ada58a51df17d7d3563b42e0a1eb
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-08-29 13:04:02 +0530

    mvc

---
+++ HelloWorldMVCWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using HelloWorldMVCWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloWorldMVCWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // http://domainname.com/home/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult abcd()
+        {
+            //return "<h1>Hello World!</h1>";
+            return View();
+        }
+    }
+}
---
+++ HelloWorldMVCWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWorldMVCWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
