18377962f01cdf5656067349029e133bbffaa92a
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-09-06 12:38:29 +0530

    wpf

---
+++ WpfAppDemo1/SimpleCalc.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace WpfAppDemo1
+{
+    /// <summary>
+    /// Interaction logic for SimpleCalc.xaml
+    /// </summary>
+    public partial class SimpleCalc : Window
+    {
+        public SimpleCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(textBox.Text);
+            int sno = int.Parse(textBox1.Text);
+            int sum = fno + sno;
+            MessageBox.Show($"The sum of {fno} and {sno} is {sum}");
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            int fno = int.Parse(textBox.Text);
+            int sno = int.Parse(textBox1.Text);
+            int mul = fno * sno;
+            MessageBox.Show($"The product of {fno} and {sno} is {mul}");
+        }
+    }
+}
---
+++ WpfLayoutsDemo/NestedLayoutWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace WpfLayoutsDemo
+{
+    /// <summary>
+    /// Interaction logic for NestedLayoutWindow.xaml
+    /// </summary>
+    public partial class NestedLayoutWindow : Window
+    {
+        public NestedLayoutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // add
+            int a = int.Parse(fno.Text);
+            int b = int.Parse(second.Text);
+            int sum = a + b;
+            result.Text = $"The sum of {a} and {b} is {sum}";
+            result.FontSize = 14;
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            // multiply
+            int a = int.Parse(fno.Text);
+            int b = int.Parse(second.Text);
+            int p = a * b;
+            result.Text = $"The product of {a} and {b} is {p}";
+
+        }
+    }
+}
