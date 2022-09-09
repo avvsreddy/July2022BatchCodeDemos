03270790ae55be5a636e02dc58056984975b8388
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-09-09 10:47:35 +0530

    wpf

---
+++ WpfDataBinding1/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfDataBinding1.Entities
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+        public string Photo { get; set; }
+    }
+}
---
+++ WpfDataBinding1/Obj2Element.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using WpfDataBinding1.Entities;
+
+namespace WpfDataBinding1
+{
+    /// <summary>
+    /// Interaction logic for Obj2Element.xaml
+    /// </summary>
+    public partial class Obj2Element : Window
+    {
+        public Obj2Element()
+        {
+            InitializeComponent();
+
+            Person p = new Person { FirstName = "Sachin", MiddleName = "Ramesh", LastName = "Tendulkar", Age = 45, Location = "Mumbai", Photo = "Images/sachin.gif" };
+            this.DataContext = p;
+        }
+    }
+}
