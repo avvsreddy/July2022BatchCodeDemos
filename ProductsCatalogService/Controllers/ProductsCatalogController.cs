29b5499be8bc2558afd832c37ed366368f4ec290
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-10-17 13:32:54 +0530

    api

---
+++ ProductsCatalogService/Controllers/ProductsCatalogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsCatalogService.Model.Data;
+using ProductsCatalogService.Model.Entities;
+
+namespace ProductsCatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsCatalogController : ControllerBase
+    {
+        IProductsCatalogRepository repo = new ProeductsCatalogEFRepository();
+
+        //MVC...ProductsCatalog/GetProducts
+        //Web API  GET .../api/ProductsCatalog
+
+        //[HttpGet]
+        public List<Product> getassdsdf()
+        {
+            return repo.GetProducts();
+        }
+
+        // get products by id
+        // GET ....api/productscatalog/234
+
+        [HttpGet]
+        [Route("{id}")]
+        public Product GetProduct(int id)
+        {
+            return repo.GetProduct(id);
+        }
+
+        // design the suitable URI and create the action methods for the below
+
+        // get products by color
+        // get products by country
+        // get products by availability
+        // get producgs by brand
+        // get products by catagory
+        // get products by price
+        // get costliest product
+        // get cheapest product
+
+    }
+}
--- ProductsCatalogService/Model/Entities/Product.cs
+++ ProductsCatalogService/Model/Entities/Product.cs
@@ -1,8 +1,11 @@
-﻿namespace ProductsCatalogService.Model.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsCatalogService.Model.Entities
 {
     public class Product
     {
         public int ProductID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int Price { get; set; }
         public string Catagory { get; set; }
